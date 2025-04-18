1d7a430d4d8b707c9afa706d62b0b1a8a0a5054d
Ray Liu <rayliu0712@gmail.com>
2025-04-18 19:46:09 +0800

    onclick

--- src/home.py
+++ src/home.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import thorpy as tp
+from typing import Callable
 
 
 class Home:
@@ -7,19 +8,23 @@ class Home:
         self.screen = screen
         self.run()
 
-    def run(self):
+    def run(self) -> None:
         title = tp.OutlinedText('ERIKA', 50)
+        title.at_unclick = lambda: print('start')
         title.center_on(self.screen)
 
-        def makeImgBtn(path: str) -> tp.ImageButton:
+        def makeImgBtn(path: str, onClick: Callable[[], None]) -> tp.ImageButton:
             img = pg.image.load(path)
             scaledImg = pg.transform.scale(img, (50, 50))
-            return tp.ImageButton('', scaledImg)
+            imgBtn = tp.ImageButton('', scaledImg)
+            imgBtn.at_unclick = onClick
+            return imgBtn
 
-        settingsBtn = makeImgBtn('assets/image/gear.png')
+        settingsBtn = makeImgBtn(
+            'assets/image/gear.png', lambda: print('setting'))
         settingsBtn.set_bottomleft(25, self.screen.get_height()-25)
 
-        closeBtn = makeImgBtn('assets/image/close.png')
+        closeBtn = makeImgBtn('assets/image/close.png', lambda: print('close'))
         closeBtn.set_topright(self.screen.get_width()-25, 25)
 
         group = tp.Group([title, settingsBtn, closeBtn], mode=None)
