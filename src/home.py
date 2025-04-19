a4cf58f8e71efd5016d619442b50df89299c633f
Ray Liu <rayliu0712@gmail.com>
2025-04-19 18:40:02 +0800

    tp.exit_app

--- src/home.py
+++ src/home.py
@@ -29,9 +29,8 @@ class Home:
             'assets/image/gear.png', lambda: print('settings'))
         settingsBtn.set_bottomleft(25, self.screen.get_height()-25)
 
-        # "1/0" should be replace by exit function, but i still not found yet
         closeBtn = makeImgBtn('assets/image/close.png',
-                              lambda: 1/0)  # raise ZeroDivisionError
+                              tp.exit_app)
         closeBtn.set_topright(self.screen.get_width()-25, 25)
 
         group = tp.Group([title, playBtn, settingsBtn, closeBtn], mode=None)
